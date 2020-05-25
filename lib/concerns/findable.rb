a818d4a3c755bb19467b693ace7fed9a989b293d
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-25 16:26:35 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -5,6 +5,10 @@ module Concerns::Findable
 
   def find_or_create_by_name(name)
     self.find_by_name(name) || self.create(name)
-  end
+     if self.find_by_name(name)
+           self.find_by_name(name)
+         else
+         song = self.create(name)
+         end
 
 end

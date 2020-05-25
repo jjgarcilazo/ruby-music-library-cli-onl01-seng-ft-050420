d11749a7083d4830bd80e54c4777fbd119bfa39d
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-25 14:36:47 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -7,7 +7,7 @@ class Song
   @@all = []
 
  def initilize(new_song,artist=nil,genre=nil)
-   @name = name
+   @name = song
    self.artist=(artist) if artist != nil
    self.genere=(genre) if genre != nil
  end
